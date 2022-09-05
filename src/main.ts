11cdcd2663a9e03457a854f18b098a05c60f7b9b
altbdoor <lancersupraskyline@gmail.com>
2022-09-05 19:01:45 +0800

    minor optimizations on angularjs runtime

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,27 @@
-import { ILocationProvider, module, route } from 'angular';
+import { ICompileProvider, ILocationProvider, module, route } from 'angular';
 import ngRoute from 'angular-route';
+import { componentName as GenerateComponentName, GenerateComponent } from './components/generate/generate.component';
 import { componentName as ScanComponentName, ScanComponent } from './components/scan/scan.component';
 import './style.css';
-import { componentName as GenerateComponentName, GenerateComponent } from './components/generate/generate.component';
 
 module('SampleApp', [ngRoute])
     .component(ScanComponentName, new ScanComponent())
     .component(GenerateComponentName, new GenerateComponent())
-    .config(($routeProvider: route.IRouteProvider, $locationProvider: ILocationProvider) => {
-        $locationProvider.hashPrefix('');
-        $routeProvider
-            .when('/scan', { template: '<scan></scan>' })
-            .when('/generate', { template: '<generate></generate>' })
-            .otherwise('/scan');
-    })
+    .config(
+        (
+            $routeProvider: route.IRouteProvider,
+            $locationProvider: ILocationProvider,
+            $compileProvider: ICompileProvider
+        ) => {
+            $locationProvider.hashPrefix('');
+            $routeProvider
+                .when('/scan', { template: '<scan></scan>' })
+                .when('/generate', { template: '<generate></generate>' })
+                .otherwise('/scan');
+
+            $compileProvider.debugInfoEnabled(false);
+            $compileProvider.commentDirectivesEnabled(false);
+            $compileProvider.cssClassDirectivesEnabled(false);
+        }
+    )
     .run(() => {});
